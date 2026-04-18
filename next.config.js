4e35286c0b464c0d91d890b208adbbae9529193a
Bishal Budhakshetri <id.kshetri@gmail.com>
2026-04-19 02:31:00 +0545

    Configure Next.js static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',   // 👈 tells Next.js to generate static files
+  output: 'export',
 }
 
 module.exports = nextConfig
